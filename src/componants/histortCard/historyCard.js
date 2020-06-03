2ca44618d114d76df71c5dd92c2aeb6523519ad6
Mahmoud Medhat <mohmoudmedhat@gmail.com>
2020-06-04 01:05:16 +0200

    post card without animation

--- src/componants/histortCard/historyCard.js
+++ src/componants/histortCard/historyCard.js
@@ -1,26 +1,13 @@
 import React, { useState } from 'react';
 import './historyCardStyle.css';
 import PropTypes from 'prop-types';
-
-
-function DeleteIcon(props) {
-
-    return (
-        <svg width={31} height={41} viewBox="0 0 31 41" fill="none" {...props}>
-            <path
-                d="M2.601 35.702c0 2.445 1.929 4.445 4.286 4.445H24.03c2.357 0 4.286-2 4.286-4.445V9.035H2.6v26.667zM30.46 2.37h-7.5L20.816.147H10.1L7.958 2.369h-7.5v4.444h30V2.37z"
-                fill="#FF4141"
-            />
-        </svg>
-    )
-};
-
-
+import DeleteIcon from "../../assets/images/svg/delete";
+import PostCard from '../postCard/postcard';
+import lamp from '../../assets/images/lamp.jpg'
 HistoryCard.propTypes = {
-    // You can declare that a prop is a specific JS type. By default, these
-    // are all optional.
+
     name: PropTypes.string,
-    data: PropTypes.string, //instanceOf(Date),
+    date: PropTypes.string, //instanceOf(Date),
     title: PropTypes.string,
     description: PropTypes.string,
     logo: PropTypes.number,
@@ -32,35 +19,53 @@ HistoryCard.propTypes = {
 function HistoryCard(props) {
     const [toggle, show] = useState(0);
     return (
-        <div className="historyCardContainer">
-            <div className="historyUserInfoContainer">
-                <div className="historyUserImage">
-                    <img src={props.image} style={{ width: '100%', height: '100%', borderRadius: '50%' }} />
+        <div style={{marginBottom:'20px'}}>
+            <div className="historyCardContainer"
+                style={!toggle ? { transition: 'all 0.5s' } :
+                    {
+                        borderBottomRightRadius: '0px',
+                        borderBottomLeftRadius: '0px',
+                        transition: 'all 0.5s'
+                    }}>
+                <div className="historyUserInfoContainer">
+                    <div className="historyUserImage">
+                        <img src={props.image} style={{ width: '100%', height: '100%', borderRadius: '50%' }} />
+                    </div>
+                    <div className="historyUserInfo" >
+                        <div className="historyUserName">{props.name}</div>
+                        <div className="historyPostDate">{props.date}</div>
+                    </div>
+
                 </div>
-                <div className="historyUserInfo" >
-                    <div className="historyUserName">{props.name}</div>
-                    <div className="historyPostDate">{props.date}</div>
+
+                <div className="historyPostInfo">
+                    <div className="historyPostDetails"><span style={{ fontWeight: '600' }}>Title:</span> {props.title}</div>
+                    <div className="historyPostDetails"><span style={{ fontWeight: '600' }}>description:</span> {props.description}</div>
                 </div>
 
-            </div>
-
-            <div className="historyPostInfo">
-                <div className="historyPostDetails"><span style={{ fontWeight: '600' }}>Title:</span> {props.title}</div>
-                <div className="historyPostDetails"><span style={{ fontWeight: '600' }}>description:</span> {props.description}</div>
-            </div>
-
-            <a href="#" onClick={() => show(!toggle)}>
-                <div className={toggle ? "historyViewButton" : "historyViewButtonActive"}>
-                    view post
-            </div>
-            </a>
-            <div className="historyDeleteIcon">
-                <a href="##" onClick={() => { }}>
-                    <DeleteIcon />
+                <a href="#" onClick={() => show(!toggle)}>
+                    <div className={toggle ? "historyViewButtonActive" : "historyViewButton"}>
+                        <p className="viewMore"> view{toggle ? " less" : " more"} </p>
+                    </div>
                 </a>
-            </div>
+                <div className="historyDeleteIcon">
+                    <a href="##" onClick={() => { }}>
+                        <DeleteIcon />
+                    </a>
+                </div>
 
-        </div >
+
+            </div >
+            {toggle && <PostCard
+                style={{marginBottom:"50px"}}
+                likes={23}
+                dislikes={2}
+                rate={4.4}
+                image={lamp}
+                fullDescription={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"}
+
+            />}
+        </div>
     );
 }
 
---
+++ src/componants/postCard/postcard.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './postCardStyle.css';
+import PropTypes from 'prop-types';
+
+import Like from "../../assets/images/svg/like.js";
+import DisLike from "../../assets/images/svg/dislike.js";
+
+
+
+
+
+
+
+
+
+
+PostCard.propTypes = {
+    fullDescription: PropTypes.string,
+    image: PropTypes.string,
+    rate: PropTypes.number,
+    likes: PropTypes.number,
+    dislikes: PropTypes.number,
+    cardStyle: PropTypes.object,
+
+
+};
+
+
+function PostCard(props) {
+    const [like, setlike] = useState(0);
+    const [dislike, setdislike] = useState(0)
+    console.log(props.style); 
+    return (
+        < div className = "postCardContainer" style = { props.cardStyle } >
+
+            <div className="postImageContainer">
+                <div className="postCardRate">{props.rate}</div>
+                <img src={props.image ? props.image : ''} style={{ width: '100%', height: '100%' }} />
+            </div>
+            <div className="postFullDescripstion">
+                {props.fullDescription}
+            </div>
+            <div className="postLikesContainer">
+                <div className="postlike" onClick={() => { setlike(!like, setdislike(0)) }}>
+
+                    <Like fill={like ? "#247189" : 'gray'} />
+                    <p className="postLikesNumber">{like && !dislike ? props.likes + 1 : props.likes}</p>
+
+
+                </div>
+                <div className="postDislike" onClick={() => { setdislike(!dislike, setlike(0)) }}>
+
+                    <div style={{ marginTop: '10px' }}> <DisLike fill={dislike ? "red" : 'gray'} /></div>
+                    <p className="postLikesNumber">{dislike && !like ? props.dislikes + 1 : props.dislikes}</p>
+
+                </div>
+            </div>
+
+        </div >
+
+    );
+}
+
+
+export default PostCard;
