cda928e7bb37607f5d58fd0f8ef8b4130ba93022
moahmed12samy <mohamedsamyabbas99@gmail.com>
2020-08-10 22:02:59 +0200

    update user

--- src/componants/profile/profile.js
+++ src/componants/profile/profile.js
@@ -1,71 +1,72 @@
 import './profile.css';
 import image from '../../assets/images/Capture.jpg';
 import Edit from '../../assets/images/svg/edit'
-import {UserContext} from '../../user_context';
+import { UserContext } from '../../user_context';
 import React, { useState, useEffect, useContext } from 'react';
 
-export default class Profile extends React.Component{
+export default class Profile extends React.Component {
 
 
   constructor(props) {
     super(props);
     this.state = {
       edit: false,
-        email: null,
-        password: null,
-        errors: {
-            email: 'required',
-            password: 'required',
-        },
-        showError: false
+      email: null,
+      password: null,
+      errors: {
+        email: 'required',
+        password: 'required',
+      },
+      showError: false
     };
     this.handleEdit = this.handleEdit.bind(this);
+    this.updateUser = this.updateUser.bind(this);
   }
 
   componentDidMount() {
-    
-    const {user, setUser} = this.context
-    console.log(JSON.stringify(this.props.user) +"2222111");
+
+    const { user, setUser } = this.context
+    console.log(JSON.stringify(this.props.user) + "2222111");
     //setUser(151);
-      this.setState({
-       user : user
-     });
-     //console.log(user+"///////") // { name: 'Tania', loggedIn: true }
-   }
-  handleEdit = () => {   
-        this.setState({
-          edit: !this.state.edit        
-      }
-        );
+    this.setState({
+      user: user
+    });
+    //console.log(user+"///////") // { name: 'Tania', loggedIn: true }
+  }
+  handleEdit = () => {
+    this.setState({
+      edit: !this.state.edit
+    }
+    );
   }
 
-  updateUser(id){
+  updateUser(id) {
     fetch(`/API/userUpdate/${id}/`, {
       "method": "PUT",
-      "headers": {'Content-Type': 'application/json',   },
+      "headers": { 'Content-Type': 'application/json', },
       "body": JSON.stringify({
-        userName: document.getElementById("username").value == '' ? document.getElementById("username").defaultValue:
-          document.getElementById("username").value ,
-        password: document.getElementById("password").value == '' ? document.getElementById("password").defaultValue:
+        userName: document.getElementById("profileusername").value == '' ? document.getElementById("profileusername").defaultValue :
+          document.getElementById("profileusername").value,
+        password: document.getElementById("password").value == '' ? document.getElementById("password").defaultValue :
           document.getElementById("password").value,
         //userType: this.state.usertype,
-        userEmail: document.getElementById("email").value == '' ? document.getElementById("email").defaultValue:
-        document.getElementById("email").value,
+        userEmail: document.getElementById("email").value == '' ? document.getElementById("email").defaultValue :
+          document.getElementById("email").value,
         userFirstName:
-        document.getElementById("username").value.split(" ")[0] == '' ? 
-        document.getElementById("username").defaultValue.split(" ")[0]:
-          document.getElementById("username").value.split(" ")[0]
-        
+          document.getElementById("profileusername").value.split(" ")[0] == '' ?
+            document.getElementById("profileusername").defaultValue.split(" ")[0] :
+            document.getElementById("profileusername").value.split(" ")[0]
+
         ,
-        userLastName: 
-        document.getElementById("username").value.split(" ")[1] == '' ? 
-        document.getElementById("username").defaultValue.split(" ")[1]:
-          document.getElementById("username").value.split(" ")[1]
-        
-        ?? 
-        document.getElementById("username").value.split(" ")[0] == '' ? 
-        document.getElementById("username").defaultValue.split(" ")[0]:
-          document.getElementById("username").value.split(" ")[0]
+        userLastName:
+          document.getElementById("profileusername").value.split(" ")[1] == '' ?
+            document.getElementById("profileusername").defaultValue.split(" ")[1] :
+            document.getElementById("profileusername").value.split(" ")[1]
+
+              ??
+              document.getElementById("profileusername").value.split(" ")[0] == '' ?
+              document.getElementById("profileusername").defaultValue.split(" ")[0] :
+              document.getElementById("profileusername").value.split(" ")[0]
         ,
         //userLastVistedCategoryID: 1
       })
@@ -89,71 +90,76 @@ export default class Profile extends React.Component{
       });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className="profile_container">
         <div className="profile_content">
           <div className="imge_name">
             <div className="profile_image">
-                <img src={image}/>
+              <img src={image} />
             </div>
             <span className="username">{this.props.user.userName}</span>
           </div>
 
           <div className="card_content">
-            
-            <form  className="form_edit">
-                <div className="input_line">
-                      <label>username</label>
-                    <input className="edit_input" name = "email" type="text"  id="username"
-                    disabled= {this.state.edit ?  "": "disabled"}
-                      defaultValue= {this.props.user.userName}
-                    />
-                 
-                </div>
 
-                <div className="input_line">
-                      <label>email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                    <input className="edit_input" name = "email" type="text"  id = "email"
-                    disabled= {this.state.edit ?  "": "disabled"}
-                    defaultValue={this.props.user.userEmail}
-                    />
-                </div>
+            <form className="form_edit">
+              <div className="input_line">
+                <label>username</label>
+                <input
+                  id = "profileusername"
+                  className="edit_input" name="username" type="text"
+                  disabled={this.state.edit ? "" : "disabled"}
+                  defaultValue={this.props.user.userName}
 
-                <div className="input_line">
-                      <label>Password</label>
-                    <input id="password" className="edit_input" name = "email" type={this.state.edit ? "text" : "password" }
-                    disabled= {this.state.edit ?  "": "disabled"}
-                    defaultValue={this.props.user.password}
-                    />
-                  
-                </div>
+                />
 
-              </form>
-              <div style={{display: "flex", alignItems:"center" }}>
-                <div type= "submit" style= {{display: this.state.edit ? "none" : "", width:"5vw", height:"4vh", marginLeft:  "20vw"  ,backgroundColor:"#3A7F95", marginTop:"1vh", marginRight:"2vw"}}
-                 onClick={this.handleEdit}> 
-                    Edit
-                  </div>
-                  
-                  <div type= "submit" style= {{display: this.state.edit ? "" : "none", width:"5vw", height:"4vh", marginLeft:"17vw"  ,backgroundColor:"#3A7F95", marginTop:"1vh", marginRight:"2vw"}}
-                 onClick={this.updateUser(this.state.user.id)}> 
-                    Submit
+              </div>
+
+              <div className="input_line">
+                <label>email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                <input className="edit_input" name="email" type="text" id="email"
+                  disabled={this.state.edit ? "" : "disabled"}
+                  defaultValue={this.props.user.userEmail}
+                />
+              </div>
+
+              <div className="input_line">
+                <label>Password</label>
+                <input id="password" className="edit_input" name="password" type={this.state.edit ? "text" : "password"}
+                  disabled={this.state.edit ? "" : "disabled"}
+                  defaultValue={this.props.user.password}
+                />
+
+              </div>
+
+            </form>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <div type="submit" style={{ display: this.state.edit ? "none" : "", width: "5vw", height: "4vh", marginLeft: "20vw", backgroundColor: "#3A7F95", marginTop: "1vh", marginRight: "2vw" }}
+                onClick={this.handleEdit}>
+                Edit
                   </div>
 
-                  <div  type= "submit" style= {{width:"5vw", height:"4vh", backgroundColor:"#3A7F95", marginTop:"1vh", 
-                  display: this.state.edit ? "inline-block" : 'none' }} onClick={this.handleEdit}> 
-                    Cancel
+              <div type="submit" style={{ display: this.state.edit ? "" : "none", width: "5vw", height: "4vh", marginLeft: "17vw", backgroundColor: "#3A7F95", marginTop: "1vh", marginRight: "2vw" }}
+                onClick={()=> this.updateUser(25)}>
+                Submit
                   </div>
 
-                  
-                </div>
-                
+              <div type="submit" style={{
+                width: "5vw", height: "4vh", backgroundColor: "#3A7F95", marginTop: "1vh",
+                display: this.state.edit ? "inline-block" : 'none'
+              }} onClick={this.handleEdit}>
+                Cancel
+                  </div>
+
+
+            </div>
+
           </div>
-         
+
         </div>
-       
+
       </div>
     );
-}
+  }
 }
